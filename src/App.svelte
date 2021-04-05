643fe0f0e5cf11a5727acba800a9772b59ade6fa
Adsnipers <adsnipers@outlook.com>
2021-04-05 17:14:36 +1000

    Welcome message reacts to time of day

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,20 @@
 	$: hour = date.getHours();
 	$: min = date.getMinutes();
 	let AMorPM = 'AP';
+	let welcomeMessage = 'Welcome To HOS'
 	
 	onMount( () => {
 		const interval = setInterval(() => {
 			date = new Date();
+			console.log(hour)
 			AMorPM = (hour >= 12) ? "PM" : "AM";
+			if (hour >= 17) {
+				welcomeMessage = "Good Evening"
+			} else if (hour >= 12) {
+				welcomeMessage = "Good Afternoon"
+			} else if (hour >= 0) {
+				welcomeMessage = "Good Morning"
+			}
 		}, 1000);
 	});
 </script>
@@ -17,6 +26,6 @@
 <body id="body">
 	<div class="time">
 		<h1 id="time">{hour - 12}:{min}{AMorPM}</h1>
-		<h1 id="msg">Welcome To HOS</h1>
+		<h1 id="msg">{welcomeMessage}</h1>
 	</div>
 </body>
